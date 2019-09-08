b6872c32407add7e6a8782e0544073c316ea3535
Freddy Martinez Garcia <freddy311082@gmail.com>
2019-09-08 14:05:46 -0300

    Not revealed cells can be revealed

--- minesweeper/tests.py
+++ minesweeper/tests.py
@@ -11,3 +11,10 @@ class CellTests(TestCase):
 
         mine_cell = CellObjectFactory.instance(CellObjectType.MINE, row=0, col=0, state=CellState.REVEALED)
         self.assertFalse(mine_cell.can_be_revealed())
+
+    def test_EmptyCell_NonRevealedCellsCanBeRevealed(self):
+        empty_cell = CellObjectFactory.instance(CellObjectType.EMPTY, row=0, col=0, state=CellState.HIDED)
+        self.assertTrue(empty_cell.can_be_revealed())
+
+        mine_cell = CellObjectFactory.instance(CellObjectType.MINE, row=0, col=0, state=CellState.HIDED)
+        self.assertTrue(mine_cell.can_be_revealed())
